4a6417816ff0752f2b2a42d1cce64941d67fda29
@timileyin42 <akanmuibro@gmail.com>
2024-06-27 06:25:09 +0000

    add

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  // Override toString to return the airport code
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
+
+export default Airport;
